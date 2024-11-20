b1cd4ca5059e15c69c58d171ca2b2243d50a9647
Nick Vuci <03_replays_rating@icloud.com>
2024-11-20 17:52:13 -0500

    Added a settings button to clean up landing page

---
+++ modal.js
@@ -0,0 +1,18 @@
+// Modal JavaScript
+const modal = document.getElementById("settingsModal");
+const btn = document.getElementById("openSettingsButton");
+const span = document.getElementsByClassName("close")[0];
+
+btn.onclick = function() {
+    modal.style.display = "block";
+}
+
+span.onclick = function() {
+    modal.style.display = "none";
+}
+
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
